440d26ef79b725e8d6b1c78ee0524bd4022b3faf
L123 <56485327+Lwsy123@users.noreply.github.com>
2025-06-30 21:35:03 +0800

    Delete maincode/net/Globalpool directory

--- maincode/net/Globalpool/GlobalPool.py
+++
@@ -1,8 +0,0 @@
-import  torch 
-from torch import nn 
-
-class GlobalMaxPool1d(nn.Module):
-    def __init__(self):
-        super(GlobalMaxPool1d,self).__init__()
-    def forward(self, x):
-        return torch.max_pool1d(x,kernel_size=x.shape[2])
